77fbcf0be0cf8257e84fe1336b3b7d5cc38d39e9
Yash Bhambhani <bhambhani.yash@gmail.com>
2023-01-20 09:00:42 -0800

    playercard updated

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
-import backgroundImage from './images/background-card.jpg'
+import cardBackground from "../images/background-card.jpg";
 
 const PlayerCard = () => {
     const [playerName, setPlayerName] = useState('Mike')
     const [backgroundImg, setBackgroundImg] = useState('')
     const [flagImage, setFlagImage] = useState('')
     const [playerImage, setPlayerImage] = useState('')
-
+    // console.log(cardBackground)
 
     return(
         <>
         <div>
             <div style={{width: "350px", height: "500px",
              borderColor: "white", borderRadius: "3px", borderStyle: "solid",
-             backgroundImage: "url(" + {backgroundImage} +")"}}>
+             backgroundImage: "url("+{cardBackground}+")",
+             backgroundSize: 'cover' }}>
+            {/* <img src={{cardBackground}}/> */}
 
             </div>
         </div>
