755892f4bea6e263d5fdb66ced35fb34028afef3
Ryan Luong <ryanluong83@gmail.com>
2023-01-25 08:49:38 -0800

    Using url for background image

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import cardBackground from "../images/background-card.jpg";
+// import cardBackground from "./images/background-card.jpg";
 
 const PlayerCard = () => {
     const [playerName, setPlayerName] = useState('Mike')
@@ -7,16 +7,13 @@ const PlayerCard = () => {
     const [flagImage, setFlagImage] = useState('')
     const [playerImage, setPlayerImage] = useState('')
     // console.log(cardBackground)
-
+    // let image = 'https://cdn.discordapp.com/attachments/693920868220403742/1064964877787086979/background-card.jpg'
     return(
         <>
         <div>
             <div style={{width: "350px", height: "500px",
-             borderColor: "white", borderRadius: "3px", borderStyle: "solid",
-             backgroundImage: "url("+{cardBackground}+")",
-             backgroundSize: 'cover' }}>
-            {/* <img src={{cardBackground}}/> */}
-
+             borderColor: "white", backgroundImage: `url("https://via.placeholder.com/500")` }}>
+            "HI"
             </div>
         </div>
         </>
