bf5734f2f9b9db89d3aab01785ad1ff5d0eddd12
Ryan Luong <ryanluong83@gmail.com>
2023-02-28 11:55:10 -0800

    Player image and background image added to the top

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -13,8 +13,10 @@ const PlayerCard = () => {
         <div>
             <div style={{ width: "350px", height: "500px", flexDirection: "column", display: "flex",
              borderColor: "white", backgroundImage: `url("https://via.placeholder.com/500")` }}>
-                <div style={{flex: 3.5, backgroundColor: "red"}}>
-                Hi
+                <div style={{flex: 3.5, backgroundColor: "red", backgroundImage: "url(https://media.discordapp.net/attachments/693920868220403742/1080207383088877668/player-card-top-05.jpg?width=824&height=468)",
+                backgroundSize: "cover"}}>
+                    <img style={{width: "200px", height: "200px"}} src="https://media.discordapp.net/attachments/693920868220403742/1080205853686251631/image.png">
+                    </img>
                 </div>
 
                 <div style={{flex: 1, backgroundColor: "gray", textAlign: "center"}}>
